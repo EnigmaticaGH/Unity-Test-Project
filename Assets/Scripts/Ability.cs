ac14ea05087e75c9cb4a33574ec7d827f3f99b4a
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2016-04-30 18:59:35 -0400

    Trying out ability system using delegates to assign functions to an ability

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class Ability
+{
+    public enum Status
+    {
+        Ready,
+        Active,
+        Cooldown
+    }
+    public string Name { get; private set; }
+    public delegate void AbilityFunction();
+    public float ActiveTime { get; private set; }
+    public float CooldownTime { get; private set; }
+    public Status AbilityStatus { get; private set; }
+    private AbilityFunction Active;
+    private Timer timer;
+
+    public Ability(string name, AbilityFunction active, float activeTime, float cooldownTime)
+    {
+        Name = name;
+        Active = active;
+        CooldownTime = cooldownTime;
+        ActiveTime = activeTime;
+        AbilityStatus = Status.Ready;
+    }
+    private void Ready(object state)
+    {
+        timer.Dispose();
+        AbilityStatus = Status.Ready;
+    }
+    public void Activate()
+    {
+        AbilityStatus = Status.Active;
+        Active();
+        timer = new Timer(Cooldown, null, (int)(ActiveTime * 1000), -1);
+    }
+    private void Cooldown(object state)
+    {
+        AbilityStatus = Status.Cooldown;
+        timer = new Timer(Ready, null, (int)(CooldownTime * 1000), -1);
+    }
+}
---
+++ Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    Ability ability;
+    void Start()
+    {
+        ability = new Ability("Ability 1", Active, 2.5f, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && ability.AbilityStatus == Ability.Status.Ready)
+        {
+            ability.Activate();
+        }
+    }
+
+    void Active()
+    {
+        StartCoroutine(AbilityActive());
+    }
+
+    IEnumerator AbilityActive()
+    {
+        float x = Random.Range(-5, 5);
+        float y = Random.Range(-5, 5);
+        float z = Random.Range(-5, 5);
+        float t = 0;
+        Vector3 oldPosition = transform.position;
+        Vector3 newPosition = transform.position + new Vector3(x, y, z);
+        while ((t += Time.deltaTime) < ability.ActiveTime)
+        {
+            yield return null;
+            transform.position = Vector3.Lerp(oldPosition, newPosition, t / ability.ActiveTime);
+        }
+    }
+}
